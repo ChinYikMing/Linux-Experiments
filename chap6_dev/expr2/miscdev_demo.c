a95558f0c5de5d3072404b190d8166d70ab2a583
ChinYikMing <yikming2222@gmail.com>
2022-01-21 00:01:10 -0800

    chapter 6 experiment 2 done

---
+++ chap6_dev/expr2/miscdev_demo.c
@@ -0,0 +1,72 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/miscdevice.h>
+
+#define MISCDEV_NAME "miscdev_demo"
+
+static int miscdev_open(struct inode *inode, struct file *f){
+	int major = MAJOR(inode->i_rdev);
+	int minor = MINOR(inode->i_rdev);
+
+	printk("%s: major: %d, minor: %d\n", MISCDEV_NAME, major, minor);
+
+	return 0;
+} 
+
+static int miscdev_release(struct inode *inode, struct file *f){
+	printk("%s is closed\n", MISCDEV_NAME);
+	return 0;
+} 
+
+static ssize_t miscdev_read(struct file *f, char __user *data, size_t count, loff_t *offset){
+	printk("%s enter\n", __func__);
+	return 0;
+} 
+
+static ssize_t miscdev_write(struct file *f, const char __user *data, size_t count, loff_t *offset){
+	printk("%s enter\n", __func__);
+	return 0;
+} 
+
+static struct file_operations miscdev_fops = {
+	.owner = THIS_MODULE,
+	.open = miscdev_open,
+	.release = miscdev_release,
+	.read = miscdev_read,
+	.write = miscdev_write
+};
+static struct miscdevice miscdev = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = MISCDEV_NAME,
+	.fops = &miscdev_fops
+};
+
+static int __init miscdev_init(void){
+	// dynamically find a major number
+	int ret;
+
+	ret = misc_register(&miscdev);
+	if(ret){
+		printk("misc_register failed\n");
+		return ret;
+	}
+
+	printk("%s dev has been created\n", MISCDEV_NAME);
+	return 0;
+}
+
+static void __exit miscdev_exit(void){
+	printk("%s has been removed\n", MISCDEV_NAME);
+	misc_deregister(&miscdev);
+}
+
+module_init(miscdev_init);
+module_exit(miscdev_exit);
+
+MODULE_AUTHOR("Ming");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("This is a simple misc device");
+
