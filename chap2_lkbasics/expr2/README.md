180e5dcb8f71f6cf30b41e67549d4f9ed305f977
ChinYikMing <yikming2222@gmail.com>
2022-09-27 08:20:21 -0700

    chapter 2 experiment 2 done

---
+++ chap2_lkbasics/expr2/README.md
@@ -0,0 +1,1 @@
+list.h from a gist: https://gist.github.com/roychen/1710968
---
+++ chap2_lkbasics/expr2/main.c
@@ -0,0 +1,37 @@
+#include "list.h"
+#include <assert.h>
+#include <stdlib.h>
+
+struct data {
+	int val;
+	struct list_head list;
+};
+
+struct data *data_new(int val){
+	struct data *data = malloc(sizeof(struct data));
+	if(!data)
+		return NULL;
+
+	data->val = val;
+	INIT_LIST_HEAD(&data->list);
+
+	return data;
+}
+
+int main(){
+	struct list_head list = LIST_HEAD_INIT(list);
+
+	struct data *data = NULL;
+	for(int i = 1; i <= 100; i++){
+		data = data_new(i);
+		assert(data != NULL);
+
+		list_add(&data->list, &list);
+	}
+
+	struct list_head *iter;
+	list_for_each(iter, &list){
+		data = list_entry(iter, struct data, list);
+		printf("data->val: %d\n", data->val);
+	}
+}
